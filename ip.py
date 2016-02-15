43f521293ddce7b58d13ffcdcf8efb5fa6fcb748
Fredrik Wigsnes <fredrik.wigsnes@gmail.com>
2016-02-15 23:56:38 +0100

    enhanced printing

--- ip.py
+++ ip.py
@@ -3,6 +3,13 @@ import argparse
 def toBinary(numb):
     return str(bin(int(numb)))[2:]
 
+def fancyPrint(add):
+    print('|',end="")
+    for x in range(4):
+        print(t.format(numbers[x],len(binary[x])),end="")
+        if x < 3:
+            print('.',end='')
+    print('|')
 def Main():
     parser = argparse.ArgumentParser(description='shows info about the ip')
     parser.add_argument('IP',nargs='+',help='the ip-address')
@@ -13,18 +20,23 @@ def Main():
     for x in numbers:
         binary.append(toBinary(x))
 
-    t = '|{:>{}s}.{:>{}s}.{:>{}s}.{:>{}s}|'
-    print(t.format(numbers[0],len(binary[0]),
-                   numbers[1],len(binary[1]),
-                   numbers[2],len(binary[2]),
-                   numbers[3],len(binary[3])))
+    t = '{:>{}s}'
 
-    print(args.IP[1])
+    print('|',end="")
+    for x in range(4):
+        print(t.format(numbers[x],len(binary[x])),end="")
+        if x < 3:
+            print('.',end='')
+    print('|')
 
-    print(t.format(toBinary(numbers[0]),len(binary[0]),
-                   toBinary(numbers[1]),len(binary[1]),
-                   toBinary(numbers[2]),len(binary[2]),
-                   toBinary(numbers[3]),len(binary[3])))
+    print('|',end="")
+    for x in range(4):
+        print(t.format(toBinary(numbers[x]),len(binary[x])),end="")
+        if x < 3:
+            print('.',end='')
+    print('|')
+
+    #print(args.IP[1])
 
 if __name__ == '__main__':
     Main()
